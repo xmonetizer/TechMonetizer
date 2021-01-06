82608b7a48bd0baf0414b2acecbf20ed40460745
stefian <v_ianta@yahoo.com>
2021-01-06 08:14:34 -0400

    team social terra dev web office cloud monetizer engine

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="welcome">
     <p>
-      <a href="https://MetaEngine.web.app/" title="Meta Engine web app"> meta </a> 
-      <a href="https://SocialsXL.web.app/" title="Social Engine web app"> social </a>
-      <a href="https://TerraDevs.web.app/" title="Terra Devs web app"> terra </a>
-      <a href="https://XLrDNA.web.app/" title="xl rDNA web app"> xl </a>
-      <a href="https://JSrDNA.web.app/" title="js rDNA web app"> js </a>
-      <a href="https://DevEngineXL.web.app/" title="Dev Engine web app"> devops </a>
+      <a href="https://TeamMonetizer.web.app/" title="Team Interests Monetizer"> team </a> 
+      <a href="https://SIMonetizer.web.app/" title="Social Interests Monetizer"> social </a>
+      <a href="https://TerraMonetizer.web.app/" title="Terra Interests Monetizer"> terra </a>
+      <a href="https://devMonetizer.web.app/" title="Devs Interests Monetizer"> dev </a>
+      <a href="https://w3Monetizer.web.app/" title="js rDNA web app"> web </a>
+      <a href="https://DevEngineXL.web.app/" title="Dev Engine web app"> office </a>
       <a href="https://CloudEngineXL.web.app/" title="Cloud Engine web app"> cloud </a>
-      <a href="https://MigrationsPM.web.app/" title="Migrations PM web app"> migration </a>
-      <a href="https://EngineXL.web.app/" title="Engine XL web app"> engine </a>
+      <a href="https://TimeMonetizer.web.app/" title="Time Monetizer"> monetizer </a>
+      <a href="https://SprintMonetizer.web.app/" title="Sprint Monetizer"> engine </a>
       </p>
     <h1>monetize @ AI speed</h1>
     <p><span class="red-text">group</span> >> <span class="yellow-text">innovate</span> >> <span class="blue-text">monetize</span> 24/7 - 3rd gen DNA OS</p>
